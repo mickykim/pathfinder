aefb7e131dd2f09434a1a58d778fb757d51e1408
Micky Kim <miqueaskimhan@gmail.com>
2022-09-17 21:02:38 -0500

    Refactored helper functions, started pathfinding algs

--- pages/PathfindingAlgorithms.tsx
+++ pages/PathfindingAlgorithms.tsx
@@ -1,5 +1,42 @@
 import { GridNode } from "./PathfinderGrid";
 
-export const dijkstras = (grid: GridNode[][]) => {};
+export const dijkstras = (grid: GridNode[][], startLocation: string) => {
+  let x = Number(startLocation.split("_")[1]);
+  let y = Number(startLocation.split("_")[2]);
+  let done = false;
+  let xOffsets = [1, -1, 0, 0];
+  let yOffsets = [0, 0, 1, -1];
+  while (!done) {
+    if (grid[x][y].visited) {
+    }
+
+    /**
+     * Checks if the node is out of bounds or already visited.
+     * If not it updates the shortest distance to the node if a
+     * shorter distance is found and updates the route.
+     *
+     * Uses offsets arrays to create temporary adjacency list of the current node.
+     * */
+    for (let i = 0; i < 4; i++) {
+      if (
+        grid[x + xOffsets[i]][y + yOffsets[i]] &&
+        !grid[x + xOffsets[i]][y + yOffsets[i]].visited
+      ) {
+        if (
+          grid[x][y].distance + 1 <
+          grid[x + xOffsets[i]][y + yOffsets[i]].distance
+        ) {
+          grid[x + xOffsets[i]][y + yOffsets[i]].distance =
+            grid[x][y].distance + 1;
+          grid[x + xOffsets[i]][y + yOffsets[i]].shortestPath = [
+            ...grid[x][y].shortestPath,
+            grid[x][y].id,
+          ];
+        }
+      }
+    }
+    grid[x][y].visited = true;
+  }
+};
 
 export const astar = () => {};
