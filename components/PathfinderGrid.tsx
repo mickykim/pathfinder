88639728ee7f788051cbac536f379f4ff95413e9
Micky Kim <miqueaskimhan@gmail.com>
2022-11-28 12:18:21 -0600

    Testing rate limit values

--- components/PathfinderGrid.tsx
+++ components/PathfinderGrid.tsx
@@ -59,7 +59,7 @@ const PathfinderGrid = ({
    * @returns Grid data double array
    */
 
-  const FUNCTION_RATE_LIMITER = 30;
+  const FUNCTION_RATE_LIMITER = 50;
 
   const canvasWrapper = useRef<HTMLDivElement>(null);
   const [grid, setGrid] = useState<GridNode[][]>([]);
